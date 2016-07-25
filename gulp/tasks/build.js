30e8243102a6e2a48cad5160ad493ec0a031a34f
Marcell Pék <marcellpek0@gmail.com>
2016-07-25 22:28:31 +0200

    Add default gulp task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,4 +1,19 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var path = require('path');
+var config = require('../config');
 
-gulp.task('build', function() { runSequence(['img', 'css']); });
+gulp.task('build', function() { runSequence(['img', 'css', 'js']); });
+
+gulp.task('default', ['build'], function() {
+  gulp.watch(path.join(config.WWW_DIR, 'sass/repfinder.scss'), function() {
+    gulp.run('css');
+  });
+
+  gulp.watch(
+      path.join(config.WWW_DIR, 'img/**/*'), function() { gulp.run('img'); });
+
+  gulp.watch(
+      path.join(config.WWW_DIR, 'js/**/*.js'), function() { gulp.run('js'); });
+
+});
